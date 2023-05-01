fd44a080a5c1bb9538eecc621283e1bd9840adb5
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2023-05-01 17:46:06 +0600

    oject literal

---
+++ src/object.ts
@@ -0,0 +1,16 @@
+const user1: {
+    company:"Programming Hero",
+    name: string,
+    age: number,
+    isMarried: boolean,
+    wife?: string
+} = {
+    company: "Programming Hero",
+    name: "Montu Mia",
+    age: 27,
+    isMarried: true,
+    wife: "bibi"
+}
+
+
+// user1.company = "Programming Hero Bangladesh";
