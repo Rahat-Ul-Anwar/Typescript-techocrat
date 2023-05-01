97b1520a188600d8a46bba414942cb87aa258cf4
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2023-05-01 13:00:56 +0600

    primitive data type

---
+++ src/primitive-types.ts
@@ -0,0 +1,44 @@
+// const name: string = "Syed Md Rahat Ul Anwar";
+let nameOfThePerson: string = "Syed Md Rahat Ul Anwar";
+
+nameOfThePerson='Jhinuk'
+// const rollNumber: number = 123;
+let rollNumber: number = 123;
+
+rollNumber = 666;
+
+rollNumber.toFixed();
+
+
+
+
+// const isAdmin:boolean = true;
+let isAdmin: boolean = true;
+
+isAdmin = false;
+
+// let course:string;
+// course = "Next level web development";
+
+// let course = undefined;
+// course = 'bla';
+// course = true;
+// course = 333;
+
+const course = undefined;
+course = 'bla';
+course = true;
+course = 333;
+
+
+
+
+
+
+
+//primitive types
+//string
+//number
+//boolean
+//null
+//undefined
